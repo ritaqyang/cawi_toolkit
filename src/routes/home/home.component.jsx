fccfe5088f28f724ee47460958992b5893cd77fb
ritaqyang <ritaqyang@gmail.com>
2024-03-02 15:09:06 -0500

    started homepage

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,16 +1,39 @@
-import React from "react";
-import PDFViewer from "../../components/PDFViewer.component";
 
+import React from 'react';
+import { HomePageWrapper, Header, Section, PortfolioItems, PortfolioItem } from './home.styles'; 
 
-
-function Home() {
+import img from '../../assets/homepage.png'; 
+const Home = () => {
     return (
-        <div>
-
-            <h1>this is home page </h1>
-           
-
-        </div>
+        <HomePageWrapper>
+            <Header>
+                <h1>Welcome to My Website</h1>
+            </Header>
+            <Section>
+                <h2>About Me</h2>
+                <p>This is where you can introduce yourself and provide some information about you or your website.</p>
+            </Section>
+            <Section>
+                <h2>Services</h2>
+                <p>Here you can list the services you offer or the main features of your website.</p>
+            </Section>
+            <Section>
+                <h2>Portfolio</h2>
+                <PortfolioItems>
+                    
+                    <PortfolioItem>
+                        <img src={img} alt="Portfolio Item" />
+                        <h3>Portfolio Item Title</h3>
+                        <p>Description of portfolio item.</p>
+                    </PortfolioItem>
+                </PortfolioItems>
+            </Section>
+            <footer>
+                <p>Footer content goes here.</p>
+            </footer>
+        </HomePageWrapper>
     );
-}
-export default Home; 
+};
+
+export default Home;
+
---
+++ src/routes/home/home.styles.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const HomePageWrapper = styled.div`
+  display: flex; 
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+export const Header = styled.header`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const Section = styled.section`
+  margin-bottom: 40px;
+`;
+
+export const PortfolioItems = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const PortfolioItem = styled.div`
+  margin-bottom: 20px;
+  flex: 0 0 calc(33.33% - 20px);
+  max-width: calc(33.33% - 20px);
+
+  @media (max-width: 768px) {
+    flex: 0 0 calc(50% - 20px);
+    max-width: calc(50% - 20px);
+  }
+
+  @media (max-width: 576px) {
+    flex: 0 0 100%;
+    max-width: 100%;
+  }
+`;
