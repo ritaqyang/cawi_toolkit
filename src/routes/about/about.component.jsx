0228dfa3dcca6b8d3656d487cbcf6a262097a644
Injoon <injoon.youn@gmail.com>
2024-03-05 16:08:09 -0500

    changed to css, fixed navbar and about page

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { Fragment } from 'react';
-import {
-    AboutTitle,
-    AboutWrapper,
-    AboutContent,
-} from './about.styles';
 import backgroundImage from '../../assets/about-bg.png';
 import gsap from 'gsap';
 import './about.css';
@@ -14,38 +9,15 @@ import { useLayoutEffect, useRef } from "react";
 gsap.registerPlugin(ScrollTrigger);
 
 function About(){ 
-    let sections = gsap.utils.toArray(".page");
-
-    gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".wrapper",
-        pin: true,
-        scrub: 1,
-        snap: 1 / (sections.length - 1),
-        // base vertical scrolling on how wide the container is so it feels more natural.
-        end: "+=3500",
-    }
-    });
 
     return (
-        <AboutWrapper className="wrapper">
-            <div style={{
-                backgroundImage:`url(${backgroundImage})`,
-                backgroundSize: '50%',
-                backgroundPosition: 'center bottom',
-                backgroundRepeat: 'no-repeat',
-                position: 'absolute',
-                width: '200%',
-                height: '50vh',
-                bottom: '0',
-            }}>
-            </div>
-            <section className="page">
-                <AboutTitle>ABOUT US</AboutTitle>
-                <AboutContent>
-                    The Canadian Advisory of Women Immigrants (CAWI) is a community organization with the 
+        <div className="content">
+            <div className="wrapper">
+                <section className="title">
+                    <h1>ABOUT US</h1>
+                </section>
+                <section className="about">
+                    <p>The Canadian Advisory of Women Immigrants (CAWI) is a community organization with the 
                     mission to empower immigrant women and girls across Canada. In 2021, the Sexual & Reproductive Health 
                     (SRH) Campaign team at CAWI received a small fund from the Regina Public Interest Research Group to 
                     conduct a community-based research project on the experiences of immigrant women and girls with the 
@@ -55,12 +27,10 @@ function About(){
                     a toolkit, develop workshops, and host an event on the work of the SRH team. 
 
                     To learn more about the work CAWI does and the work of the SRH team at CAWI, please visit us at 
-                    www.cawicanada.com or at our social media channels.
-                </AboutContent>
-            </section>
-            <section className="page">
-                <AboutContent>
-                    This toolkit and curriculum has been funded by Oxfam Canada (in partnership with the Government of Canada) 
+                    www.cawicanada.com or at our social media channels.</p>
+                </section>
+                <section className="ack">
+                    <p>This toolkit and curriculum has been funded by Oxfam Canada (in partnership with the Government of Canada) 
                     and the Regina Public Interest Research Group. We are grateful for their generosity and support through 
                     various stages of the current project. We would also like to thank all the organizations and community 
                     members who participated in community consultations for the toolkit, including Alisa Tukkimäki from the 
@@ -69,10 +39,12 @@ function About(){
 
                     We would like to acknowledge and thank the volunteers at the Canadian Advisory of Women Immigrants (CAWI), 
                     who have dedicated their time and energy to this toolkit and curriculum. To learn who has contributed, 
-                    please refer to the list below containing names of volunteers from CAWI. 
-                </AboutContent>
-            </section>
-        </AboutWrapper>
+                    please refer to the list below containing names of volunteers from CAWI.</p>
+                </section>
+                <section className="authors">
+                </section>
+            </div>
+        </div>
         
     ); 
 }
