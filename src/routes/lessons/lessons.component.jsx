0cfe39fd1963c943225e8f72f5af5d512e9bca43
Injoon <injoon.youn@gmail.com>
2024-03-14 20:14:35 -0400

    lessons page styling

--- src/routes/lessons/lessons.component.jsx
+++ src/routes/lessons/lessons.component.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, {useLayoutEffect} from "react";
 import img from '../../assets/homepage.png';
 import {  LessonPageContainer, SidebarContainer,SidebarContent, MainContent } from "./lessons.styles"
 import LessonGrid from "../../components/lessons-page/modules-grid/modulegrid.component";
 import { PdfDisplay } from "../../components/lessons-page/pdf-download/pdfdisplay.component";
+import { gsap } from "gsap";
 
 
 const modules = [
-    { id: 1, name: 'Module 1', description: 'What is Sexual & Reproductive Health?', image: img },
-    { id: 2, name: 'Module 2', description: 'Navigating Relationships', image: img },
-    { id: 3, name: 'Module 3', description: 'Consent & Boundaries', image: img },
-    { id: 4, name: 'Module 4', description: 'Anti-Racism & Anti-Oppression in SRH', image: img },
-    { id: 5, name: 'Module 5', description: 'LGBTQ2S & Queer Communities', image: img },
-    { id: 6, name: 'Module 6', description: 'Creating Safe Spaces', image: img },
+    { id: 1, name: 'MODULE 1', description: 'What is Sexual & Reproductive Health?', image: img },
+    { id: 2, name: 'MODULE 2', description: 'Navigating Relationships', image: img },
+    { id: 3, name: 'MODULE 3', description: 'Consent & Boundaries', image: img },
+    { id: 4, name: 'MODULE 4', description: 'Anti-Racism & Anti-Oppression in SRH', image: img },
+    { id: 5, name: 'MODULE 5', description: 'LGBTQ2S & Queer Communities', image: img },
+    { id: 6, name: 'MODULE 6', description: 'Creating Safe Spaces', image: img },
 
 
 ];
@@ -25,25 +26,12 @@ const modules = [
 
 const LessonsHome = ()  => {
 
-   
     return (
         <div>
             <LessonPageContainer>
-                <SidebarContainer>
-                    <SidebarContent>
-                        <h2>Sidebar</h2>
-                        <ul>
-                            <li>Link 1</li>
-                            <li>Link 2</li>
-                            <li>Link 3</li>
-                        </ul>
-
-                    </SidebarContent>
-                    
-                </SidebarContainer>
+                
                 <MainContent>
                     <LessonGrid modules={modules} />
-                    <PdfDisplay />
 
                 </MainContent>
                
