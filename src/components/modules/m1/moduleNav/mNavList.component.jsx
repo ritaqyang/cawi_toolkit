6682fc59d8d15feb9ab29bfdea42682932d5fc8d
Injoon <injoon.youn@gmail.com>
2024-03-20 18:59:35 -0400

    clean up components

--- src/components/modules/m1/moduleNav/mNavList.component.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Outlet, Link } from 'react-router-dom';
-
-const ModuleNavList = ({}) => {
-    
-}
-
-export default ModuleNavList;
