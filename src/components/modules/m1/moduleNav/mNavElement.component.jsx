86b0a8a8e7cb9e90d111c0541cf9d5a4e0073854
Injoon <injoon.youn@gmail.com>
2024-04-01 14:42:52 -0400

    make ModuleNav component

--- src/components/modules/m1/moduleNav/mNavElement.component.jsx
+++ src/components/modules/m1/moduleNav/mNavElement.component.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Outlet, Link } from 'react-router-dom';
-import './mNavElement.css';
+import './moduleNav.styles.css';
 
 const ModuleNavElement = ({isActive, onClick, to, children}) => {
 
--- src/components/modules/m1/moduleNav/moduleNav.component.jsx
+++ src/components/modules/m1/moduleNav/moduleNav.component.jsx
@@ -1,34 +1,74 @@
 import React, { useRef, useState } from "react";
+import { Outlet, Link } from 'react-router-dom';
 import ModuleNavElement from './mNavElement.component';
+import './moduleNav.styles.css';
+import {gsap} from "gsap";
+
+const ModuleNav = ({ navElements, activeOption, handleDisplayOptionChange, contentRef }) => {
+    const navRef = useRef(null);
+    const hideRef = useRef(null);
+    const showRef = useRef(null);
+    const navElRef = useRef(null);
 
 
+    const handleNavHide = () => {
+        gsap.to(navElRef.current, {
+            height: '0px',
+        })
+        gsap.to(navRef.current, {
+            width: '5%',
+            delay: 0.5
+        })
+        gsap.to(contentRef.current, {
+            width: '100%',
+            delay: 0.5
+        })
+        gsap.to(showRef.current, {
+            display: 'inline'
+        })
+        gsap.to(hideRef.current, {
+            display: 'none'
+        })
+    }
 
-const ModuleNav = ({topics}) => {
-    const topicRefs = useRef(topics.map(() => React.createRef())); // Create refs using React.createRef()
-    const [activeOption, setActiveOption] = useState(topicRefs.current[0].current);
+    const handleNavShow = () => {
+        gsap.to(contentRef.current, {
+            width: '80%'
+        })
+        gsap.to(navRef.current, {
+            width: '20%'
+        })
+        gsap.to(navElRef.current, {
+            height: 'auto',
+            delay: 0.5
+        })
+        gsap.to(showRef.current, {
+            display: 'none',
+        })
+        gsap.to(hideRef.current, {
+            display: 'inline'
+        })
+    }
 
-    const handlePageChange = (ref) => {
-        setActiveOption(ref.current);
-        if (ref.current) {
-            ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-    };
     return (
-        <section className="m-nav">
-                <ul>
-                {topics.map((topic, index) => (
-                        <ModuleNavElement
-                            key={index}
-                            isActive={activeOption === topicRefs.current[index].current}
-                            onClick={() => handlePageChange(topicRefs.current[index])}
-                            to="#"
-                        >
-                            {topic}
-                        </ModuleNavElement>
-                    ))}
-                </ul>
+        <section className="m-nav" ref={navRef}>
+        <div className="hide-and-show">
+                <Link className="hide-link" ref={hideRef} onClick={handleNavHide} to="#">HIDE</Link>
+                <Link className="show-link" ref={showRef} onClick={handleNavShow} to="#">SHOW</Link>
+        </div> 
+        <ul className="nav-elements" ref={navElRef}>
+            {navElements.map((navElement, index) => (
+                <ModuleNavElement 
+                    key={index} 
+                    isActive={activeOption === navElement.option} 
+                    onClick={() => handleDisplayOptionChange(navElement.option)}
+                >
+                    {navElement.label}
+                </ModuleNavElement>
+            ))}
+        </ul>
         </section>
     );
-}
+};
 
 export default ModuleNav;
