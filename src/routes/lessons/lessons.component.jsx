8a4fc77399474d27e0db7104d62780ca4b4aaae9
Rita Yang <ritaqyang@gmail.com>
2024-06-27 09:26:26 -0400

    uploaded images for lessons page

--- src/routes/lessons/lessons.component.jsx
+++ src/routes/lessons/lessons.component.jsx
@@ -3,11 +3,12 @@ import img from '../../assets/homepage.png';
 import {  LessonPageContainer, SidebarContainer,SidebarContent, MainContent } from "./lessons.styles"
 import LessonGrid from "../../components/lessons-page/modules-grid/modulegrid.component";
 import { gsap } from "gsap";
+import m2 from '../../assets/lessons-page/m2_img.png';
 
 
 const modules = [
     { id: 1, name: 'MODULE 1', description: 'What is Sexual & Reproductive Health?', image: img },
-    { id: 2, name: 'MODULE 2', description: 'Navigating Relationships', image: img },
+    { id: 2, name: 'MODULE 2', description: 'Navigating Relationships', image: m2 },
     { id: 3, name: 'MODULE 3', description: 'Consent & Boundaries', image: img },
     { id: 4, name: 'MODULE 4', description: 'Anti-Racism & Anti-Oppression in SRH', image: img },
     { id: 5, name: 'MODULE 5', description: 'LGBTQ2S & Queer Communities', image: img },
