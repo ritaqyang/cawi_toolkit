121d94b8b091b83284ffe304008c7cdfea11c755
ritaqyang <ritaqyang@gmail.com>
2024-07-03 14:54:35 -0400

    meeting notes

--- src/components/modules/moduleNav/mNavElement.component.jsx
+++ src/components/modules/moduleNav/mNavElement.component.jsx
@@ -9,6 +9,10 @@ const ModuleNavElement = ({isActive, onClick, to, children}) => {
             <Link className={isActive ? "topic-link active" : "topic-link"} to={to} onClick={onClick}>
                 {children}
             </Link>
+            {/* /** completion box (take an argument that memorizes the state of the course completion
+            (consider user, module, moduleindex ))
+            onclick = change box to green with checkmark
+            another function: increment progress value  */ }
         </li>
     );
 }
